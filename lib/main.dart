5c5e3bf0a58f58231bc532b0a1230c004ee67f19
Melani <melanisulistyowati25@gmail.com>
2025-06-30 11:13:16 +0700

    rename register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:application_ivote/register.dart';
+import 'package:application_ivote/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
