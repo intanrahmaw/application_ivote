701c1c5eb4a7c71b872ab86c81ed285096ec7ac6
intanrahmaw <intanrhmaa12@gmail.com>
2025-06-30 11:16:53 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:application_ivote/splash_screen.dart';
 import 'package:application_ivote/welcome1_screen.dart';
 import 'package:application_ivote/welcome2_screen.dart';
 import 'package:application_ivote/welcome3_screen.dart';
-import 'package:application_ivote/register.dart';
+import 'package:application_ivote/registrasi_screen.dart';
 import 'package:application_ivote/dashboard.dart';
 
 void main() {
