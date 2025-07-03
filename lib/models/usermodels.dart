e3e20ac674881a6083724299913ddde83fc32926
Melani <melanisulistyowati25@gmail.com>
2025-07-03 21:30:40 +0700

    membuat usercrud

---
+++ lib/models/usermodels.dart
@@ -0,0 +1,51 @@
+class Profile {
+  final String userId;
+  final String username;
+  final String password; // ⚠️ Sebaiknya dihindari jika di frontend
+  final String nama;
+  final String email;
+  final String alamat;
+  final String noHp;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Profile({
+    required this.userId,
+    required this.username,
+    required this.password,
+    required this.nama,
+    required this.email,
+    required this.alamat,
+    required this.noHp,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+      userId: json['user_id'],
+      username: json['username'],
+      password: json['password'],
+      nama: json['nama'],
+      email: json['email'],
+      alamat: json['alamat'] ?? '',
+      noHp: json['no_hp'] ?? '',
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: DateTime.parse(json['updated_at']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'user_id': userId,
+      'username': username,
+      'password': password,
+      'nama': nama,
+      'email': email,
+      'alamat': alamat,
+      'no_hp': noHp,
+      'created_at': createdAt.toIso8601String(),
+      'updated_at': updatedAt.toIso8601String(),
+    };
+  }
+}
