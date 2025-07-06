f3b215b9486b08d49288f11a07f7a71187f2aa12
Melani <melanisulistyowati25@gmail.com>
2025-07-07 00:01:42 +0700

    constanta dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 // lib/utils/constants.dart
 
 // GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
-const String supabaseUrl = 'https://epoclgzuaffbhnptofgx.supabase.co';
-const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwb2NsZ3p1YWZmYmhucHRvZmd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDA2NTIsImV4cCI6MjA2NTU3NjY1Mn0.EI88eAG-ky1k0shiXYjQQC5bvx0L-LOvXdiKegQT-5s';
+const String supabaseUrl = 'https://pmbcqpsmjprxfcqwzrfd.supabase.co';
+const String supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtYmNxcHNtanByeGZjcXd6cmZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExOTk5MDcsImV4cCI6MjA2Njc3NTkwN30.H0Q1bpvt907Yc7Z2IW9svVcKY5VTLArRn72SsevOchA';
