eb0165762aa485f2070806a157f4c0120206c9ac
Melani <melanisulistyowati25@gmail.com>
2025-07-10 21:20:14 +0700

    menambah utilis

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+// lib/utils/constants.dart
+
+// GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
+const String supabaseUrl = 'https://epoclgzuaffbhnptofgx.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwb2NsZ3p1YWZmYmhucHRvZmd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDA2NTIsImV4cCI6MjA2NTU3NjY1Mn0.EI88eAG-ky1k0shiXYjQQC5bvx0L-LOvXdiKegQT-5s';
